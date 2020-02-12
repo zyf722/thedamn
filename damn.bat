19b4995703e5501bb84f9d585e8e4a00249be682
zyf722 <1124982772@qq.com>
2020-02-12 20:03:19 +0800

    :art: Optimize code

--- damn.bat
+++ damn.bat
@@ -1,12 +1,7 @@
 ::======================
-:: TheDamn Additional Script v0.2
+:: TheDamn Additional Script v0.3
 ::======================
 @echo off
-set DR=%CD%
-cd /d %~dp0
-doskey /HISTORY > cmd_history.log
-cd /d %DR%
+doskey /HISTORY > %~dp0cmd_history.log
 damn.py
-cd /d %~dp0
-del cmd_history.log /f /q
-cd /d %DR%
+del %~dp0cmd_history.log /f /q
